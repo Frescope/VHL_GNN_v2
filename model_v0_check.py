ab2c10495a2eaf2ea2e8c78ebc9837505586b33b
Frescope <1903323590@qq.com>
2021-02-28 09:19:55 -0500

    label_greedy

--- model_v0_check.py
+++ model_v0_check.py
@@ -64,7 +64,7 @@ DROP_OUT = hp.dropout
 EVL_EPOCHS = 1  # epochs for evaluation
 L2_LAMBDA = 0.005  # weightdecay loss
 GRAD_THRESHOLD = 10.0  # gradient threshold
-MAX_F1 = 0.6
+MAX_F1 = 0.4
 
 GPU_NUM = hp.gpu_num
 BATCH_SIZE = hp.bc
