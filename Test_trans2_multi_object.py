7d4aa7c13b51e072362d94ef8850565a0e84d6b6
Frescope <1903323590@qq.com>
2021-06-02 06:35:34 -0400

    video_trans_mob

--- Test_trans2_multi_object.py
+++ Test_trans2_multi_object.py
@@ -642,8 +642,6 @@ def main(self):
 
     # evaluate all videos in turn
     for kfold in range(4):
-        if kfold < 3:
-            continue
         # split data
         data_train = {}
         data_valid = {}
@@ -672,8 +670,6 @@ def main(self):
 
         # repeat
         for i in range(hp.repeat):
-            if i < 1:
-                continue
             model_save_dir = MODEL_SAVE_BASE + hp.msd + '_%d_%d/' % (kfold, i)
             logging.info('*' * 10 + str(i) + ': ' + model_save_dir + '*' * 10)
             logging.info('*' * 60)
